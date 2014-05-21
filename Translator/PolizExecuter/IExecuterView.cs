6685bc0dbf5b3fcfe7cf16b2c3c5fda7bc10b59c
MayRiv <MayRiv@i.ua>
2014-05-21 23:22:45 +0300

    Created interface for executer

---
+++ Translator/PolizExecuter/IExecuterView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolizInterpretator
+{
+    public interface IExecuterView
+    {
+        double GetValue();
+        string Result { get; set; }
+    }
+}
